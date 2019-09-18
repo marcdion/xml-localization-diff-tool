74b80eab2687e1c3620f458f87b8832d49c373e7
Marc-Antoine Dion <madion@spektrummedia.com>
2019-09-18 14:46:28 -0400

    Basci functionnality implemented. It is possible to compare two XML files and extract missing items from both

--- main.py
+++ main.py
@@ -9,9 +9,39 @@ src = minidom.parse(str(sys.argv[2]))
 comp = minidom.parse(str(sys.argv[3]))
 
 #fetching localization items in both xml files
-itemlistsrc = src.getElementsByTagName(nodetitle)
-itemlistcomp = comp.getElementsByTagName(nodetitle)
+itemListSrc = src.getElementsByTagName(nodetitle)
+itemListComp = comp.getElementsByTagName(nodetitle)
+
+itemListSrcValues = []
+itemListCompValues = []
 
 print(f'\nSearching nodes matching: {nodetitle}\n')
-print(f'Nodes matching descriptor in first file: {len(itemlistsrc)}')
-print(f'Nodes matching descriptor in second file: {len(itemlistcomp)}')
+print(f'Nodes matching descriptor in first file: {len(itemListSrc)}')
+print(f'Nodes matching descriptor in second file: {len(itemListComp)}')
+
+for s in itemListSrc:
+    itemListSrcValues.append(s.attributes['name'].value)
+
+for c in itemListComp:
+    itemListCompValues.append(c.attributes['name'].value)
+
+diff_list_src = list(set(itemListSrcValues)-set(itemListCompValues))
+diff_list_comp = list(set(itemListCompValues)-set(itemListSrcValues))
+
+print(f'\nNumber of nodes present in both files: {len(itemListSrc)-len(diff_list_src)}')
+print(f'Number of nodes present in first file and missing in second: {len(diff_list_src)}')
+print(f'Number of nodes in second file and missing in first: {len(diff_list_comp)}')
+
+print(f'\nNodes present in first file but missing in second:\n')
+
+count_s = 1
+for val in diff_list_src:
+    print(f'{count_s}. {val}')
+    count_s = count_s + 1
+
+print(f'\nNodes present in second file but missing in first:\n')
+
+count_c = 1
+for val in diff_list_comp:
+    print(f'{count_c}. {val}')
+    count_c = count_c + 1
