c84cfe4ed7902dbe9855fa33b6c01b053aad9b9f
Marc-Antoine Dion <madion@spektrummedia.com>
2019-09-18 14:27:29 -0400

    Added dynamic node title and adjusted terminal output

--- main.py
+++ main.py
@@ -2,15 +2,16 @@
 import sys
 from xml.dom import minidom
 
-nodetitle = 'text'
+nodetitle = sys.argv[1]
 
 #parsing files to compare them. file location will be static for the moment
-src = minidom.parse(str(sys.argv[1]))
-comp = minidom.parse(str(sys.argv[2]))
+src = minidom.parse(str(sys.argv[2]))
+comp = minidom.parse(str(sys.argv[3]))
 
 #fetching localization items in both xml files
 itemlistsrc = src.getElementsByTagName(nodetitle)
 itemlistcomp = comp.getElementsByTagName(nodetitle)
 
-print(len(itemlistsrc))
-print(len(itemlistcomp))
+print(f'\nSearching nodes matching: {nodetitle}\n')
+print(f'Nodes matching descriptor in first file: {len(itemlistsrc)}')
+print(f'Nodes matching descriptor in second file: {len(itemlistcomp)}')
